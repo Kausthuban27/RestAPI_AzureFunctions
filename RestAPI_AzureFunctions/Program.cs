d602ffa86395e5f1b0c9cdc931353972e68ad68c
Kausthuban27 <168825283+Kausthuban27@users.noreply.github.com>
2024-05-20 11:47:00 +0530

    Update Program.cs

--- RestAPI_AzureFunctions/Program.cs
+++ RestAPI_AzureFunctions/Program.cs
@@ -1,21 +1,24 @@
-using Google.Protobuf.WellKnownTypes;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using RestAPI_AzureFunctions.Interface;
-using RestAPI_AzureFunctions.Model;
-using RestAPI_AzureFunctions.Service;
+using Todo_CRUD.Interface;
+using Todo_CRUD.Model;
+using Todo_CRUD.Service;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
-    .ConfigureServices(services =>
+    .ConfigureServices((context, services) =>
     {
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
-        services.AddDbContext<TodoContext>();
         services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-        services.AddScoped<IUser, UserService>();
-        services.AddCors(options => options.AddPolicy("AllowAny", policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
+        services.AddScoped<ITodo, TodoService>();
+        var configuration = context.Configuration;
+        string connectionString = configuration.GetSection("ConnectionStrings:defaultConnection").Value!;
+        services.AddDbContext<TodoContext>(options =>
+            options.UseSqlServer(connectionString));
     })
     .Build();
 
